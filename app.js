3ade478723310f3e762939d4438b62fe00d7c29e
Sam Zudell <s.zudell98@gmail.com>
2023-03-07 09:36:40 -0500

    finished exercises

--- app.js
+++ app.js
@@ -2,3 +2,22 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+let search = document.getElementById("searchWord");
+let submitSearch = document.getElementById("submitSearch")
+let img = document.getElementById("img")
+let feedBack = document.getElementById("feedback")
+
+submitSearch.addEventListener('click', () => {
+    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=9JWE1WpHR98so9cGg64O8OtMLernrFxA&s=${search.value}`)
+        .then((res) => res.json())
+        .then((data) => {
+            img.src = data.data.images.original.url
+
+        })
+        .catch((err) => {
+            console.error(err)
+            feedBack.textContent = err.message
+        })
+    search.value = ""
+})
